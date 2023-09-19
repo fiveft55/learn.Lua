379ffc79295243d30517145eaabb962ce5d1e3c0
src alt <52785691+KoldKardio@users.noreply.github.com>
2023-09-19 16:57:08 +1000

    Update main.lua

--- calc_lua/main.lua
+++ calc_lua/main.lua
@@ -1,15 +1,5 @@
--- print("hello world!")
-
-
 -- creating a  calculator demo
 
--- variables
--- local numA = 1 
--- local numB = 2
--- variables in a single line
--- local numA,numB = 1, 5
-
--- global vars omitting local key
 io.write("Please enter your first number: ")
 numA = io.read()
 io.write("Please enter your second number: ")
@@ -18,6 +8,7 @@ numB = io.read()
 io.write("Now please type -- add, min, mul or div to operate: ")
 oper = io.read()
 
+-- declaring var with 'local' to highlight... however, omission of L is fine
 local sum = 0
 
 if oper == "add" then
@@ -29,11 +20,8 @@ elseif oper == "mul" then
 elseif oper == "div" then
     sum = numA // numB -- use '/' to get value in float or '//' in integer or round-up
 else
-    print("Wrong operation! Please choose a proper operation...")
+    print("\n Wrong operation! Please choose a proper operation...")
 end
 
 -- final output
-print("Your answer is: " .. sum)
-
--- print(oper)
--- print("your sum is: " .. sum) -- either-way ("text", sum) :: the ".." is to connect
+print("Your answer is: " .. sum) -- either ("text", sum) :: the ".." is to connect or concatenate 
